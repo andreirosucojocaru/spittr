5551e9793e2a1c7138fd076be9488ff0d11a9338
andreirosucojocaru <andrei.rosucojocaru@gmail.com>
2020-09-13 22:59:15 +0300

    * request input via query parameters

--- src/main/java/com/example/spittr/web/SpittleController.java
+++ src/main/java/com/example/spittr/web/SpittleController.java
@@ -1,11 +1,15 @@
 package com.example.spittr.web;
 
+import com.example.spittr.Spittle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.spittr.data.SpittleRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/spittles")
@@ -13,14 +17,24 @@ public class SpittleController {
 
     private SpittleRepository spittleRepository;
 
+    private static final String MAX_LONG_AS_STRING = Long.MAX_VALUE + "";
+
     @Autowired
     public SpittleController(SpittleRepository spittleRepository) {
         this.spittleRepository = spittleRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    /*@RequestMapping(method = RequestMethod.GET)
     public String spittles(Model model) {
         model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE,20));
         return "spittles";
+    }*/
+
+    @RequestMapping(method=RequestMethod.GET)
+    public List<Spittle> spittles(
+            @RequestParam(value="max",
+                    defaultValue=MAX_LONG_AS_STRING) long max,
+            @RequestParam(value="count", defaultValue="20") int count) {
+        return spittleRepository.findSpittles(max, count);
     }
 }
